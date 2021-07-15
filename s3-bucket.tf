114761e7f049ab757da27eaa3c36cb2d3eb07cf1
evan <evan@alpharho.com>
2021-07-15 18:53:12 +0000

    try 2

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/EvanT-training/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
-  bucket_prefix = "evan_tall"
+  bucket_prefix = "evan-tall"
 }
